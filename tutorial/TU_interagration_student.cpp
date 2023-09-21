c8831850c7d4104ae8e67c5df5099398fef719c3
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2023-09-21 14:17:15 +0900

    Add files via upload

--- tutorial/TU_interagration_student.cpp
+++ tutorial/TU_interagration_student.cpp
@@ -3,27 +3,28 @@
 
 Author           : [YOUR NAME]
 Created          : 17-05-2021
-Modified         : 17-05-2022
+Modified         : 17-05-2023
 Language/ver     : C++ in MSVS2019
 
-Description      : [Tutorial]Integration_student.cpp
+Description      : [Tutorial] Integration_student.cpp
 -------------------------------------------------------------------------------*/
 
-#include "myNM.h"
+// #include "myNM.h"
+
+
 
 // Integration using rectangular method for discrete data inputs
 double IntegrateRect(double x[], double y[], int m);
 
 // You need to create myFunc() in this main source file
-
 // You need to create trapz() in myNM.h. myNM.cpp
-
+// You need to create simpson13() in myNM.h. myNM.cpp
 // You need to create integral() in myNM.h. myNM.cpp
 
 
 int main(int argc, char* argv[])
 {
-	// PART 1. Integration from Datasets, may not evenly distribted
+	
 	printf("\n**************************************************");
 	printf("\n        PART 1. Integration from Datasets         ");
 	printf("\n**************************************************\n");
@@ -35,34 +36,34 @@ int main(int argc, char* argv[])
 	double I_rect = IntegrateRect(x, y, M);
 	printf("I_rect  = %f\n", I_rect);
 
+
+	// Exercise 1. Trapezoid
 	double I_trapz = 0;
 	// ADD YOUR CODE HERE  I_trapz=trapz()
 	printf("I_trapz = %f\n\n", I_trapz);
 
-	
 
-	// PART 2. Integration from Datasets with evenly distributed
-	printf("\n**************************************************");
-	printf("\n        PART 2. Integration from Datasets, may not evenly distribted       ");
-	printf("\n**************************************************\n");
-
+	// Exercise 2. Simpson
 	double I_simpson = 0;
 	// ADD YOUR CODE HERE.   I_simpson13=simpson13()
 	printf("I_simpson13  = %f\n\n", I_simpson13);
+	
 
-
-	// PART 3. Integration from a Function
+	
+	
 	printf("\n**************************************************");
-	printf("\n        PART 3. Integration from a Function       ");
+	printf("\n        PART 2. Integration from a Function       ");
 	printf("\n**************************************************\n");
 	
-	double I_simpson13 = 0;
-	// ADD YOUR CODE HERE.   I_simpson13=integral()
-	printf("I_simpson13  = %f\n\n", I_simpson13);
+	// Exercise 3. Integral
+	double I_function = 0;
+	// ADD YOUR CODE HERE.   I_function=integral()
+	printf("I_function  = %f\n\n", I_function);
 
 	system("pause");
 	return 0;
 }
+
 
 // Integration using rectangular method for discrete data inputs
 double IntegrateRect(double x[], double y[], int m) {
@@ -70,6 +71,5 @@ double IntegrateRect(double x[], double y[], int m) {
 	double I = 0;
 	for (int i = 0; i < N; i++)
 		I += y[i] * (x[i + 1] - x[i]);
-
 	return I;
 }
