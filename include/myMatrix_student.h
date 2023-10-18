1115c01165a8db06d764a0a49f6cbdf2577edf07
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2023-10-18 19:59:08 +0900

    Add files via upload

---
+++ include/myMatrix_student.h
@@ -0,0 +1,75 @@
+/*----------------------------------------------------------------\
+@ Numerical Programming by Young-Keun Kim - Handong Global University
+
+Author           : [YOUR NAME]
+Created          : 26-03-2018
+Modified         : 19-10-2023
+Language/ver     : C++ in MSVS2019
+
+Description      : myMatrix.h
+----------------------------------------------------------------*/
+
+#ifndef		_MY_MATRIX_H		// use either (#pragma once) or  (#ifndef ...#endif)
+#define		_MY_MATRIX_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+typedef struct { 
+	double** at;
+	int rows, cols;
+}Matrix;
+
+
+// Create Matrix with specified size
+extern	Matrix	createMat(int _rows, int _cols);
+
+// Free a memory allocated matrix
+extern	void	freeMat(Matrix _A);
+
+// Create a matrix from a text file
+extern	Matrix	txt2Mat(std::string _filePath, std::string _fileName);
+
+//// Print matrix
+extern	void	printMat(Matrix _A, const char* _name);
+
+// Matrix addition
+extern	Matrix	addMat(Matrix _A, Matrix _B);
+
+
+
+
+
+//////////////////////////////////////////////////////////////////
+/*							Tutorial							*/
+//////////////////////////////////////////////////////////////////
+
+
+// Create matrix of all zeros
+extern	Matrix	zeros(int _rows, int _cols);
+
+// Create matrix of all ones
+extern	Matrix	ones(int _rows, int _cols);
+
+// initialization of Matrix elements
+extern	void	initMat(Matrix _A, double _val);
+
+
+// Multiply  matrix A and matrix B
+extern	Matrix	multMat(Matrix _A, Matrix _B);
+
+// Create identity 
+extern	Matrix	eye(int _rows, int _cols);
+
+// Create Transpose matrix
+extern	Matrix	transpose(Matrix _A);
+
+// Copy matrix
+extern	Matrix	copyMat(Matrix _A);
+
+// Copy matrix Elements from A to B
+extern	void	copyVal(Matrix _A, Matrix _B);
+
+
+#endif
